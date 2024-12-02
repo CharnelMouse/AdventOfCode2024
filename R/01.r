93cd29314122ab4f3e9a0ae2ece6d676ea70fcfe
Mark Webster <markwebster204@yahoo.co.uk>
2024-12-02 13:14:22 +0000

    Tidy up day 1 R a bit

--- R/01.r
+++ R/01.r
@@ -1,6 +1,5 @@
 sorted <- scan("inputs/01.txt", list("", ""), quiet = TRUE) |>
-  lapply(as.integer) |>
-  lapply(sort)
+  lapply(\(x) sort(as.integer(x)))
 sum(mapply(\(x, y) abs(x - y), sorted[[1]], sorted[[2]])) # part one: 2742123
 matches <- outer(sorted[[1]], sorted[[2]], `==`)
 sum(matches*sorted[[1]]) # part two: 21328497
