11143869103635d6c0785a2cb8de3ffe4aa1778a
Mark Webster <markwebster204@yahoo.co.uk>
2024-12-02 09:23:28 +0000

    Add day 2 part 2 (R)

--- R/02.r
+++ R/02.r
@@ -1,13 +1,13 @@
-readLines("inputs/02.txt") |>
+x <- readLines("inputs/02.txt") |>
   strsplit(" ", fixed = TRUE) |>
-  vapply(
-    \(x) {
-      x |>
-        as.integer() |>
-        diff() |>
-        (\(y) y*sign(y[[1]]))() |>
-        (\(y) all(y >= 1 & y <= 3))()
-    },
-    logical(1)
-  ) |>
-  sum()
+  lapply(as.integer)
+safe <- function(x) {
+  x <- diff(x)
+  y <- x*sign(x[[1]])
+  all(y >= 1 & y <= 3)
+}
+sum(vapply(x, safe, logical(1))) # part one: 407
+damp_safe <- function(x) {
+  any(vapply(seq_along(x), \(n) safe(x[-n]), logical(1)))
+}
+sum(vapply(x, damp_safe, logical(1)))
