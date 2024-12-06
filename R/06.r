307587686b0179b1f3310fa7fe25235e83ff8efa
Mark Webster <markwebster204@yahoo.co.uk>
2024-12-06 15:47:27 +0000

    Add day 6 part 2 (R)

--- R/06.r
+++ R/06.r
@@ -96,13 +96,86 @@ while (!anyNA(pos)) {
 
 nrow(unique(visited[1:2]))
 
-# part two
+# part two: 1831
 
-path <- unique(visited)
+path <- unique(subset(visited[1:2], row != start[1] | col != start[2]))
 
-mult_vis <- path |>
-  aggregate(
-    dir ~ .,
-    \(x) diff(match(unique(x), c("up", "right", "down", "left"))) %% 4
-  ) |>
-  subset(lengths(dir) > 0)
+jump_or_loop <- function(pos, direction = c("up", "down", "left", "right"), obstacles, jump_starts) {
+  if (nrow(subset(jump_starts, row == pos[1] & col == pos[2] & dir == direction) > 0)) {
+    return(c(NA_integer_, NA_integer_))
+  }
+  direction <- match.arg(direction)
+  switch(
+    direction,
+    up = {
+      hit <- obstacles |>
+        subset(row < pos[1] & col == pos[2]) |>
+        getElement("row") |>
+        max() |>
+        suppressWarnings()
+      if (is.infinite(hit))
+        c(NA_integer_, pos[2])
+      else
+        c(hit + 1L, pos[2])
+    },
+    down = {
+      hit <- obstacles |>
+        subset(row > pos[1] & col == pos[2]) |>
+        getElement("row") |>
+        min() |>
+        suppressWarnings()
+      if (is.infinite(hit))
+        c(NA_integer_, pos[2])
+      else
+        c(hit - 1L, pos[2])
+    },
+    left = {
+      hit <- obstacles |>
+        subset(row == pos[1] & col < pos[2]) |>
+        getElement("col") |>
+        max() |>
+        suppressWarnings()
+      if (is.infinite(hit))
+        c(pos[1], NA_integer_)
+      else
+        c(pos[1], hit + 1L)
+    },
+    right = {
+      hit <- obstacles |>
+        subset(row == pos[1] & col > pos[2]) |>
+        getElement("col") |>
+        min() |>
+        suppressWarnings()
+      if (is.infinite(hit))
+        c(pos[1], NA_integer_)
+      else
+        c(pos[1], hit - 1L)
+    }
+  )
+}
+
+# slow
+makes_look <- vapply(
+  seq_len(nrow(path)),
+  \(n) {
+    obstacle <- as.vector(path[n, ])
+    new_obstacles <- rbind(obstacles, obstacle)
+    pos <- start
+    direction <- "up"
+    jump_starts <- data.frame(row = integer(), col = integer(), dir = character())
+    while (!anyNA(pos)) {
+      nxt <- jump_or_loop(pos, direction, new_obstacles, jump_starts)
+      jump_starts <- rbind(
+        jump_starts,
+        data.frame(row = pos[1], col = pos[2], dir = direction)
+      )
+      pos <- nxt
+      direction <- c("right", "down", "left", "up")[
+        match(direction, c("up", "right", "down", "left"))
+      ]
+    }
+    sum(is.na(pos)) == 2
+  },
+  logical(1)
+)
+sum(makes_look)
