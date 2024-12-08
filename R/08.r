b9fa612be5ff750f3fe354bf376c8ee27098ed69
Mark Webster <markwebster204@yahoo.co.uk>
2024-12-08 11:15:28 +0000

    Rearrange day 8 part 1 (R)

--- R/08.r
+++ R/08.r
@@ -8,30 +8,38 @@ antennae <- lapply(
     which(x == freq, arr.ind = TRUE)
   }
 )
-antinodes <- lapply(
-  antennae,
-  \(x) {
-    lapply(
-      seq_len(nrow(x) - 1),
-      \(n) {
-        lapply(
-          setdiff(seq_len(nrow(x)), seq_len(n)),
-          \(m) {
-            p1 <- x[n, ]
-            p2 <- x[m, ]
-            matrix(
-              c(2L*p1 - p2, 2L*p2 - p1),
-              ncol = 2,
-              byrow = TRUE
-            )
-          }
-        ) |>
-          do.call(what = rbind)
-      }
-    ) |>
-      do.call(what = rbind) |>
-      unique()
-  }
-)
-uniq <- unique(do.call(rbind, antinodes))
-nrow(uniq[apply(uniq, 1, \(pos) all(pos >= 1 & pos <= dim(x))), ]) # part one: 348
+calculate_antinodes <- function(map, antennae, pairwise_antinodes) {
+  lapply(
+    antennae,
+    \(x) {
+      lapply(
+        seq_len(nrow(x) - 1),
+        \(n) {
+          lapply(
+            setdiff(seq_len(nrow(x)), seq_len(n)),
+            \(m) {
+              pairwise_antinodes(map, x[n, ], x[m, ])
+            }
+          ) |>
+            do.call(what = rbind)
+        }
+      ) |>
+        do.call(what = rbind) |>
+        unique()
+    }
+  ) |>
+    do.call(what = rbind) |>
+    unique()
+}
+
+# part one: 348
+pairwise_simple_antinodes <- function(x, p1, p2) {
+  res <- matrix(
+    c(2L*p1 - p2, 2L*p2 - p1),
+    ncol = 2,
+    byrow = TRUE
+  )
+  res[apply(res, 1, \(y) all(y >= 1 & y <= dim(x))), , drop = FALSE]
+}
+simple_antinodes <- calculate_antinodes(x, antennae, pairwise_simple_antinodes)
+nrow(simple_antinodes)
