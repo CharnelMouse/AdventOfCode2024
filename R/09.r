456624a4df46e2e8df9a229c9e5baf6ab69b8c33
Mark Webster <markwebster204@yahoo.co.uk>
2024-12-09 12:32:18 +0000

    Tidy up day 9 (R)

--- R/09.r
+++ R/09.r
@@ -1,3 +1,6 @@
+# RLE is a compact way to track memory blocks, but requires some manual merging
+# of adjacent blocks to avoid repeatedly inverting and re-applying RLE
+
 x <- readLines("inputs/09.txt") |>
   strsplit("", fixed = TRUE) |>
   getElement(1) |>
@@ -33,12 +36,15 @@ while (any(mem_rle$values == -1)) {
     mem_rle$values[[hole_pos]] <- mem_rle$values[[block_pos]]
     mem_rle$lengths[[block_pos]] <- block_len - hole_len
   }
-  mem_rle <- rle(inverse.rle(mem_rle))
+  while (tail(mem_rle$lengths, 1) == 0 || tail(mem_rle$values, 1) == -1) {
+    mem_rle$lengths <- head(mem_rle$lengths, -1)
+    mem_rle$values <- head(mem_rle$values, -1)
+  }
 }
 
 memblock <- inverse.rle(mem_rle)
 memblock[memblock == -1] <- 0L
-sum(memblock*(seq_along(memblock) - 1L))
+format(sum(memblock*(seq_along(memblock) - 1L)), scientific = FALSE)
 
 # part two: 6221662795602
 
@@ -56,24 +62,36 @@ for (id in rev(seq_len(max(enc$values)))) {
   hole_pos <- hole_poses[insertion]
   if (hole_pos > block_pos)
     next
-  # print(paste("move", id))
   hole_len <- mem_rle2$lengths[[hole_pos]]
+  # move copy of block
   mem_rle2$lengths[[hole_pos]] <- hole_len - block_len
   mem_rle2$values[[block_pos]] <- -1L
-  mem_rle2$values <- append(mem_rle2$values, id, after = hole_pos - 1L)
   mem_rle2$lengths <- append(mem_rle2$lengths, block_len, after = hole_pos - 1L)
-  while (tail(mem_rle2$values, 1) == -1) {
-    mem_rle2$lengths <- head(mem_rle2$lengths, -1)
-    mem_rle2$values <- head(mem_rle2$values, -1)
+  mem_rle2$values <- append(mem_rle2$values, id, after = hole_pos - 1L)
+  # merge blocks on either side of old block if same value (i.e. empty)
+  # old block with right
+  if (
+    block_pos + 1 < length(mem_rle2$lengths) &&
+    mem_rle2$values[[block_pos + 1]] == mem_rle2$values[[block_pos + 2]]
+  ) {
+    mem_rle2$lengths[[block_pos + 1]] <- mem_rle2$lengths[[block_pos + 1]] + mem_rle2$lengths[[block_pos + 2]]
+    mem_rle2$lengths <- mem_rle2$lengths[-(block_pos + 2)]
+    mem_rle2$values <- mem_rle2$values[-(block_pos + 2)]
   }
-  mem_rle2 <- rle(inverse.rle(mem_rle2))
-  # print(inverse.rle(mem_rle2))
-  hole_poses <- which(mem_rle2$values == -1)
-  hole_sizes <- mem_rle2$lengths[hole_poses]
-  max_hole_size <- max(hole_sizes)
-  moveable_blocks <- which(mem_rle2$values != -1 & mem_rle2$lengths <= max_hole_size)
+  # old block with left
+  if (
+    block_pos < length(mem_rle2$lengths) &&
+    mem_rle2$values[[block_pos]] == mem_rle2$values[[block_pos + 1]]
+  ) {
+    mem_rle2$lengths[[block_pos]] <- mem_rle2$lengths[[block_pos]] + mem_rle2$lengths[[block_pos + 1]]
+    mem_rle2$lengths <- mem_rle2$lengths[-(block_pos + 1)]
+    mem_rle2$values <- mem_rle2$values[-(block_pos + 1)]
+  }
+  zero_width <- mem_rle2$lengths == 0
+  mem_rle2$lengths <- mem_rle2$lengths[!zero_width]
+  mem_rle2$values <- mem_rle2$values[!zero_width]
 }
 
 memblock2 <- inverse.rle(mem_rle2)
 memblock2[memblock2 == -1] <- 0L
-sum(memblock2*(seq_along(memblock2) - 1L), na.rm = TRUE)
+format(sum(memblock2*(seq_along(memblock2) - 1L), na.rm = TRUE), scientific = FALSE)
