35000363c08fb5bfd842a0d018b687485cad0b2d
Mark Webster <markwebster204@yahoo.co.uk>
2024-12-10 10:53:25 +0000

    Add day 10 part 2 (R)

--- R/10.r
+++ R/10.r
@@ -2,6 +2,8 @@ x <- readLines("inputs/10.txt") |>
   strsplit("", fixed = TRUE) |>
   lapply(as.integer) |>
   do.call(what = rbind)
+
+# part one: 557
 ends <- as.data.frame(which(x == 9, arr.ind = TRUE))
 trace <- transform(
   ends,
@@ -24,3 +26,24 @@ for (level in 8:0) {
 trace <- trace |>
   transform(count = lengths(ends))
 sum(trace$count)
+
+# part two: 1062
+starts <- as.data.frame(which(x == 0, arr.ind = TRUE))
+trace2 <- transform(
+  starts,
+  count = 1L
+)
+for (level in 1:9) {
+  candidates <- rbind(
+    transform(trace2, row = row - 1L),
+    transform(trace2, row = row + 1L),
+    transform(trace2, col = col - 1L),
+    transform(trace2, col = col + 1L)
+  ) |>
+    subset(row >= 1 & row <= nrow(x) & col >= 1 & col <= ncol(x))
+  trace2 <- candidates |>
+    transform(height = mapply(\(r, c) x[r, c], row, col)) |>
+    subset(height == level) |>
+    aggregate(count ~ row + col, sum)
+}
+sum(trace2$count)
