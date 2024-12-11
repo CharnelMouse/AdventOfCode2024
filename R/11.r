80c3cb0b38677ff21fffd1dd5ebbb083023d5c8c
Mark Webster <markwebster204@yahoo.co.uk>
2024-12-11 09:39:09 +0000

    Add day 11 part 2 (R)

--- R/11.r
+++ R/11.r
@@ -8,24 +8,31 @@ update <- function(stones) {
   indiv <- mapply(
     \(n, val) {
       if (val == 0)
-        return(rep(1, n))
+        return(setNames(n, 1))
       len <- nchar(val)
       if (len %% 2 == 0) {
         left <- substr(val, 1, len/2)
         right <- substr(val, len/2 + 1, len)
         res <- as.numeric(c(left, right))
-        return(rep(res, n))
+        return(setNames(c(n, n), res))
       }
       res <- val*2024
-      return(rep(res, n))
+      return(setNames(n, res))
     },
     stones,
     as.numeric(names(stones))
   )
-  table(Reduce(c, indiv, init = numeric()))
+  freqs <- Reduce(c, indiv, init = integer())
+  tapply(freqs, names(freqs), sum)
 }
 state <- table(x)
 for (n in seq_len(25)) {
+  state <- update(state)
+}
+sum(state)
+
+# part two: 221291560078593
+for (n in seq_len(50)) {
   state <- update(state)
 }
 sum(state)
