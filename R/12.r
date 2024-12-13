b39c52d5beef4f658349fb249059d2b063bd92ed
Mark Webster <markwebster204@yahoo.co.uk>
2024-12-13 09:33:15 +0000

    Add day 12 part 2 (R)

--- R/12.r
+++ R/12.r
@@ -11,7 +11,12 @@ plant_cells <- lapply(
     sort_by(~ row + col)
 )
 block <- function(cells) {
-  cells <- cbind(cells, block = NA_integer_, neighbours = NA_integer_)
+  cells <- cbind(
+    cells,
+    block = rep(NA_integer_, nrow(cells)),
+    neighbours = rep(NA_integer_, nrow(cells))
+  )
+  cells$neighbour_pos <- rep(list(NULL), nrow(cells))
   cells <- sort_by(cells, ~ row + col)
   for (n in seq_len(nrow(cells))) {
     cell <- cells[n, c("row", "col")]
@@ -24,6 +29,11 @@ block <- function(cells) {
     )
     matched_neighbours <- merge(neighbours, cells[c("row", "col", "block")])
     n_neighbours <- nrow(matched_neighbours) - 1L
+    neighbour_pos <- subset(
+      matched_neighbours,
+      row != cell[[1]] | col != cell[[2]],
+      c(row, col)
+    )
     if (all(is.na(matched_neighbours$block))) {
       matched_neighbours$new_block <- max(c(0L, cells$block), na.rm = TRUE) + 1L
     }else{
@@ -52,3 +62,54 @@ price <- function(cells) {
     with(sum(perimeter * area))
 }
 sum(vapply(plant_blocks, price, integer(1)))
+
+# part two: 923480
+blocks <- Map(
+  \(bl, nm) cbind(data.frame(plant = nm), bl),
+  plant_blocks,
+  names(plant_blocks)
+) |>
+  do.call(what = rbind)
+area <- tapply(blocks, blocks[c("plant", "block")], nrow)
+block_corners <- function(cells) {
+  if (nrow(cells) == 0)
+    return(0L)
+  neighbours <- rbind(
+    cbind(cells[c("row", "col")], dir = "W", row.n = cells$row - 1L, col.n = cells$col),
+    cbind(cells[c("row", "col")], dir = "E", row.n = cells$row + 1L, col.n = cells$col),
+    cbind(cells[c("row", "col")], dir = "N", row.n = cells$row, col.n = cells$col - 1L),
+    cbind(cells[c("row", "col")], dir = "S", row.n = cells$row, col.n = cells$col + 1L),
+    cbind(cells[c("row", "col")], dir = "NW", row.n = cells$row - 1L, col.n = cells$col - 1L),
+    cbind(cells[c("row", "col")], dir = "NE", row.n = cells$row + 1L, col.n = cells$col - 1L),
+    cbind(cells[c("row", "col")], dir = "SW", row.n = cells$row - 1L, col.n = cells$col + 1L),
+    cbind(cells[c("row", "col")], dir = "SE", row.n = cells$row + 1L, col.n = cells$col + 1L)
+  ) |>
+    merge(cells[c("row", "col")], by.x = c("row.n", "col.n"), by.y = c("row", "col")) |>
+    merge(cells[c("row", "col")], all.y = TRUE) |>
+    subset(, -c(row.n, col.n)) |>
+    transform(dir = addNA(dir))
+  # corner if two orthogonal neighbours and diagonal neighbour are empty (¬o & ¬d),
+  # or (o & ¬d) or (¬o & d), i.e. ¬o | (o & ¬d).
+  aggregate(
+    neighbours,
+    dir ~ row + col,
+    \(dirs) {
+      (!is.element("N", dirs) && !is.element("E", dirs)) +
+        (is.element("N", dirs) && is.element("E", dirs) && !is.element("NE", dirs)) +
+        (!is.element("S", dirs) && !is.element("E", dirs)) +
+        (is.element("S", dirs) && is.element("E", dirs) && !is.element("SE", dirs)) +
+        (!is.element("N", dirs) && !is.element("W", dirs)) +
+        (is.element("N", dirs) && is.element("W", dirs) && !is.element("NW", dirs)) +
+        (!is.element("S", dirs) && !is.element("W", dirs)) +
+        (is.element("S", dirs) && is.element("W", dirs) && !is.element("SW", dirs))
+    }
+  ) |>
+    getElement("dir") |>
+    sum()
+}
+corners <- tapply(
+  blocks,
+  blocks[c("plant", "block")],
+  block_corners
+)
+sum(area*corners)
